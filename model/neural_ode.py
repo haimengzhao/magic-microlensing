c632bf0a003abab217d8c6e8ad41ce16aa3c146e
hmzhao <haimengzhao@icloud.com>
2021-10-24 14:47:56 +0800

    Model: neural_ode, encoder_decoder, utils

---
+++ model/neural_ode.py
@@ -0,0 +1,95 @@
+import torch
+import torch.nn as nn
+import utils
+
+from torchdiffeq import odeint as odeint
+
+class ODEFunc(nn.Module):
+	'''
+	Module for ode function f in dy/dt = f
+
+	Modified from https://github.com/YuliaRubanova/latent_ode/blob/master/lib/ode_func.py
+	'''
+	def __init__(self, ode_func_net, device = torch.device("cpu")):
+		"""
+		input_dim: dimensionality of the input
+		latent_dim: dimensionality used for ODE. Analog of a continous latent state
+		"""
+		super(ODEFunc, self).__init__()
+
+		self.device = device
+ 
+		utils.init_network_weights(ode_func_net)
+		self.gradient_net = ode_func_net
+
+	def forward(self, t_local, y, backwards = False):
+		"""
+		Perform one step in solving ODE. Given current data point y and current time point t_local, returns gradient dy/dt at this time point
+
+		t_local: current time point
+		y: value at the current time point
+		"""
+		grad = self.gradient_net(y)
+		if backwards:
+			grad = -grad
+		return grad
+
+	# def get_ode_gradient_nn(self, t_local, y):
+	# 	return self.gradient_net(y)
+
+	# def sample_next_point_from_prior(self, t_local, y):
+	# 	"""
+	# 	t_local: current time point
+	# 	y: value at the current time point
+	# 	"""
+	# 	return self.get_ode_gradient_nn(t_local, y)
+
+
+class DiffeqSolver(nn.Module):
+	'''
+	Differential equation solver for neural-ODE
+
+	Modified from https://github.com/YuliaRubanova/latent_ode/blob/master/lib/diffeq_solver.py
+	'''
+	def __init__(self, ode_func, method, 
+			odeint_rtol = 1e-4, odeint_atol = 1e-5, device = torch.device("cpu")):
+		super(DiffeqSolver, self).__init__()
+		self.device = device
+
+		self.ode_func = ode_func
+		self.ode_method = method
+		self.odeint_rtol = odeint_rtol
+		self.odeint_atol = odeint_atol
+
+	def forward(self, initial_value, time_steps_to_predict, backwards = False):
+		"""
+		Decode the trajectory through ODE Solver
+		"""
+		n_traj_samples, n_traj = initial_value.size()[0], initial_value.size()[1]
+
+		pred_y = odeint(self.ode_func, initial_value, time_steps_to_predict, 
+			rtol=self.odeint_rtol, atol=self.odeint_atol, method = self.ode_method)
+		pred_y = pred_y.permute(1,2,0,3)
+
+		assert(torch.mean(pred_y[:, :, 0, :]  - initial_value) < 0.001)
+		assert(pred_y.size()[0] == n_traj_samples)
+		assert(pred_y.size()[1] == n_traj)
+
+		return pred_y
+
+	# def sample_traj_from_prior(self, starting_point_enc, time_steps_to_predict, 
+	# 	n_traj_samples = 1):
+	# 	"""
+	# 	Decode the trajectory through ODE Solver using samples from the prior
+
+	# 	time_steps_to_predict: time steps at which we want to sample the new trajectory
+	# 	"""
+	# 	func = self.ode_func.sample_next_point_from_prior
+
+	# 	pred_y = odeint(func, starting_point_enc, time_steps_to_predict, 
+	# 		rtol=self.odeint_rtol, atol=self.odeint_atol, method = self.ode_method)
+	# 	# shape: [n_traj_samples, n_traj, n_tp, n_dim]
+	# 	pred_y = pred_y.permute(1,2,0,3)
+	# 	return pred_y
+
+
---
+++ model/utils.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+
+def create_net(n_inputs, n_outputs, n_layers = 1, n_units = 100, nonlinear = nn.Tanh):
+    '''
+    Create a fully connected net:
+    
+    n_inputs --nonlinear-> (n_units --nonlinear-> ) * n_layers -> n_outputs
+
+    Modified from https://github.com/YuliaRubanova/latent_ode/blob/master/lib/utils.py
+    '''
+    layers = [nn.Linear(n_inputs, n_units)]
+    for i in range(n_layers):
+        layers.append(nonlinear())
+        layers.append(nn.Linear(n_units, n_units))
+
+	layers.append(nonlinear())
+	layers.append(nn.Linear(n_units, n_outputs))
+	return nn.Sequential(*layers)
+
+def init_network_weights(net, std = 0.1):
+    '''
+    Initialize network weights.
+
+    Modified from https://github.com/YuliaRubanova/latent_ode/blob/master/lib/utils.py
+    '''
+    for m in net.modules():
+        if isinstance(m, nn.Linear):
+            nn.init.normal_(m.weight, mean=0, std=std)
+            nn.init.constant_(m.bias, val=0)
+
+def get_device(tensor):
+    '''
+    Get device of tensor.
+
+    Modified from https://github.com/YuliaRubanova/latent_ode/blob/master/lib/utils.py
+    '''
+    device = torch.device("cpu")
+    if tensor.is_cuda:
+        device = tensor.get_device()
+    return device
+
+def split_last_dim(data):
+    '''
+    Split the last dim of data into halves.
+
+    Modified from https://github.com/YuliaRubanova/latent_ode/blob/master/lib/utils.py
+    '''
+    last_dim = data.size()[-1]
+    last_dim = last_dim//2
+
+    if len(data.size()) == 3:
+        res = data[:,:,:last_dim], data[:,:,last_dim:]
+
+    if len(data.size()) == 2:
+        res = data[:,:last_dim], data[:,last_dim:]
+    return res
