c71f958b0dc99bf4cc14ae8d0a51ab932464ba59
hmzhao <haimengzhao@icloud.com>
2022-01-14 18:25:53 +0800

    Model: locator

---
+++ model/locator.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn as nn
+
+import model.utils as utils
+
+import torchcde
+
+class LocatorCDEFunc(nn.Module):
+    '''
+    Locator's neural CDE grad function.
+
+    Ref: https://github.com/patrick-kidger/torchcde/blob/master/example/time_series_classification.py
+    '''
+    def __init__(self, input_dim, latent_dim):
+        super(LocatorCDEFunc, self).__init__()
+        self.input_dim = input_dim
+        self.latent_dim = latent_dim
+
+        self.linear1 = nn.Linear(latent_dim, 1024)
+        self.relu1 = nn.ReLU()
+        self.resblocks = nn.Sequential(*[utils.ResBlock(1024, 1024, nonlinear=nn.ReLU, layernorm=False) for i in range(3)])
+        self.relu2 = nn.ReLU()
+        self.linear2 = nn.Linear(1024, input_dim * latent_dim)
+        self.tanh = nn.Tanh()
+        self.linear3 = nn.Linear(input_dim * latent_dim, input_dim * latent_dim)
+    
+    def forward(self, t, z):
+        # z = torch.cat([t.repeat(z.shape[0], 1), z], dim=-1)
+        z = self.linear1(z)
+        z = self.relu1(z)
+        z = self.resblocks(z)
+        z = self.relu2(z)
+        z = self.linear2(z)
+        z = self.tanh(z) # important!
+        z = self.linear3(z)
+
+        z = z.view(z.shape[0], self.latent_dim, self.input_dim)
+
+        return z
+
+
+
+class Locator(nn.Module):
+    '''
+    A Neural CDE Locator.
+
+    Ref: https://github.com/patrick-kidger/torchcde/blob/master/example/time_series_classification.py
+    '''
+    def __init__(self, input_dim, latent_dim, output_dim):
+        super(Locator, self).__init__()
+        self.input_dim = input_dim
+        self.latent_dim = latent_dim
+
+        self.cde_func = LocatorCDEFunc(input_dim, latent_dim)
+        utils.init_network_weights(self.cde_func, method=nn.init.orthogonal_)
+        self.initial = nn.Sequential(
+            utils.create_net(input_dim, 1024, n_layers=0, n_units=1024, nonlinear=nn.ReLU),
+            *[utils.ResBlock(1024, 1024, nonlinear=nn.ReLU) for i in range(3)],
+            utils.create_net(1024, latent_dim, n_layers=0, n_units=1024, nonlinear=nn.ReLU),
+        )
+        self.readout = nn.Sequential(
+            utils.create_net(latent_dim, 1024, n_layers=0, n_units=1024, nonlinear=nn.ReLU),
+            *[utils.ResBlock(1024, 1024, nonlinear=nn.ReLU) for i in range(3)],
+            nn.Linear(1024, output_dim)
+        )
+       
+    def forward(self, coeffs):
+        X = torchcde.CubicSpline(coeffs)
+
+        X0 = X.evaluate(X.interval[0])
+        z0 = self.initial(X0)
+
+        z_T = torchcde.cdeint(X=X,
+                              z0=z0,
+                              func=self.cde_func,
+                              t=X.interval,
+                              adjoint=False,
+                              method="dopri5", rtol=1e-5, atol=1e-7)
+
+        z_T = z_T[:, -1]
+        pred = self.readout(z_T)
+        return pred
---
+++ train_locator.py
@@ -0,0 +1,245 @@
+import os
+import sys
+
+import argparse
+import numpy as np
+from random import SystemRandom
+import h5py
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+import model.utils as utils
+from model.locator import Locator
+
+import torchcde
+
+from tensorboardX import SummaryWriter
+
+parser = argparse.ArgumentParser('Locator')
+parser.add_argument('--niters', type=int, default=1000)
+parser.add_argument('--lr',  type=float, default=4e-6, help="Starting learning rate")
+parser.add_argument('-b', '--batch-size', type=int, default=128)
+
+parser.add_argument('--dataset', type=str, default='/work/hmzhao/irregular-lc/work/hmzhao/irregular-lc/roman-0.h5', help="Path for dataset")
+parser.add_argument('--save', type=str, default='/work/hmzhao/experiments/locator/', help="Path for save checkpoints")
+parser.add_argument('--load', type=str, default=None, help="ID of the experiment to load for evaluation. If None, run a new experiment.")
+parser.add_argument('--resume', type=int, default=0, help="Epoch to resume.")
+parser.add_argument('-r', '--random-seed', type=int, default=42, help="Random_seed")
+
+parser.add_argument('-l', '--latents', type=int, default=32, help="Dim of the latent state")
+
+args = parser.parse_args()
+
+device = torch.device("cuda:6" if torch.cuda.is_available() else "cpu")
+file_name = os.path.basename(__file__)[:-3]
+utils.makedirs(args.save)
+
+#####################################################################################################
+
+if __name__ == '__main__':
+    torch.manual_seed(args.random_seed)
+    np.random.seed(args.random_seed)
+    os.environ['JOBLIB_TEMP_FOLDER'] = '/work/hmzhao/tmp'
+
+    print(f'Num of GPUs available: {torch.cuda.device_count()}')
+
+    experimentID = args.load
+    if experimentID is None:
+        # Make a new experiment ID
+        experimentID = int(SystemRandom().random() * 100000)
+    print(f'ExperimentID: {experimentID}')
+    ckpt_path = os.path.join(args.save, "experiment_" + str(experimentID) + '.ckpt')
+    
+    input_command = sys.argv
+    ind = [i for i in range(len(input_command)) if input_command[i] == "--load"]
+    if len(ind) == 1:
+        ind = ind[0]
+        input_command = input_command[:ind] + input_command[(ind+2):]
+    input_command = " ".join(input_command)
+
+    writer = SummaryWriter(log_dir=f'/work/hmzhao/tbxdata/locator-{experimentID}')
+
+    ##################################################################
+    print(f'Loading Data: {args.dataset}')
+    with h5py.File(args.dataset, mode='r') as dataset_file:
+        Y = torch.tensor(dataset_file['Y'][...])
+        X_even = torch.tensor(dataset_file['X_even'][...])
+        X_rand = torch.tensor(dataset_file['X_random'][...])
+
+    test_size = 1024
+    train_size = len(Y) - test_size
+    # train_size = 128 * 16
+
+    # preprocess
+    # Y: t_0, t_E, u_0, rho, q, s, alpha, f_s
+    Y = Y[:, [0, 1, -1]] # locator predicts t_0, t_E and f_s
+    Y[:, -1] = torch.log10(Y[:, -1])
+    
+    # discard uncertainty bar
+    X_rand = X_rand[:, :, :2]
+ 
+    # CDE interpolation with log_sig
+    depth = 3; window_length = 72; window_length_rand = 72
+    train_logsig = torchcde.logsig_windows(X_even[:train_size, :, :], depth, window_length=window_length)
+    train_coeffs = torchcde.hermite_cubic_coefficients_with_backward_differences(train_logsig)
+
+    train_logsig_rand = torchcde.logsig_windows(X_rand[:train_size, :, :], depth, window_length=window_length_rand)
+    train_rand_coeffs = torchcde.hermite_cubic_coefficients_with_backward_differences(train_logsig_rand)
+
+    train_dataset = torch.utils.data.TensorDataset(train_coeffs, Y[:train_size])
+    train_rand_dataset = torch.utils.data.TensorDataset(train_rand_coeffs, Y[:train_size])
+
+    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=False)
+    train_rand_dataloader = DataLoader(train_rand_dataset, batch_size=args.batch_size, shuffle=False)
+
+    train_mix_dataset = torch.utils.data.TensorDataset(torch.cat([train_coeffs, train_rand_coeffs], dim=0), Y[:train_size].repeat(2, 1))
+    train_mix_dataloader = DataLoader(train_mix_dataset, batch_size=args.batch_size, shuffle=True)
+
+    test_logsig = torchcde.logsig_windows(X_even[(-test_size):, :, :].float().to(device), depth, window_length=window_length)
+    test_coeffs = torchcde.hermite_cubic_coefficients_with_backward_differences(test_logsig)
+    test_Y = Y[(-test_size):].float().to(device)
+    
+    test_logsig_rand = torchcde.logsig_windows(X_rand[(-test_size):, :, :].float().to(device), depth, window_length=window_length_rand)
+    test_rand_coeffs = torchcde.hermite_cubic_coefficients_with_backward_differences(test_logsig_rand).float().to(device)
+
+    output_dim = Y.shape[-1]
+    input_dim = train_logsig.shape[-1]
+    latent_dim = args.latents
+
+    del Y
+    del X_even
+    del X_rand
+    ##################################################################
+    # Create the model
+    model = Locator(input_dim, latent_dim, output_dim).to(device)
+    ##################################################################
+    # Load checkpoint and evaluate the model
+    if args.load is not None:
+        # Load checkpoint.
+        checkpt = torch.load(ckpt_path, map_location='cpu')
+        ckpt_args = checkpt['args']
+        state_dict = checkpt['state_dict']
+        model_dict = model.state_dict()
+
+        # 1. filter out unnecessary keys
+        state_dict = {k: v for k, v in state_dict.items() if k in model_dict}
+        # 2. overwrite entries in the existing state dict
+        model_dict.update(state_dict) 
+        # 3. load the new state dict
+        model.load_state_dict(model_dict)
+        model.to(device)
+    ##################################################################
+    # Training
+    print('Start Training Locator')
+    log_path = "logs/" + file_name + "_" + str(experimentID) + ".log"
+    utils.makedirs("logs/")
+    
+    logger = utils.get_logger(logpath=log_path, filepath=os.path.abspath(__file__))
+    logger.info(input_command)
+    logger.info("Experiment Locator" + str(experimentID))
+
+    optimizer = optim.Adam(
+        [
+            {"params": model.initial.parameters(), "lr": args.lr*1e2},
+            {"params": model.cde_func.parameters(), "lr": args.lr},
+            {"params": model.readout.parameters(), "lr": args.lr*1e2},
+        ],
+        lr=args.lr
+        )
+    # optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+    # optimizer = optim.SGD(model.parameters(), lr=args.lr)
+
+    num_batches = len(train_dataloader)
+
+    loss_func = nn.MSELoss()
+
+    for epoch in range(args.resume, args.resume + args.niters):
+        utils.update_learning_rate(optimizer, decay_rate = 0.99, lowest = args.lr / 10)
+        lr = optimizer.state_dict()['param_groups'][0]['lr']
+        print(f'Epoch {epoch}, Learning Rate {lr}')
+        writer.add_scalar('learning_rate', lr, epoch)
+        
+        # if epoch % 2 == 0:
+        #     e_dataloader = train_dataloader
+        #     print('Using regular data')
+        # else:
+        #     e_dataloader = train_rand_dataloader
+        #     print('Using irregular data')
+        e_dataloader = train_mix_dataloader
+        num_batches = len(e_dataloader)
+            
+        for i, (batch_coeffs, batch_y) in enumerate(e_dataloader):
+
+            batch_y = batch_y.float().to(device)
+            batch_coeffs = batch_coeffs.float().to(device)
+
+            optimizer.zero_grad()
+
+            pred_y = model(batch_coeffs)
+
+            mse = torch.mean((batch_y - pred_y)**2, dim=-1).detach().cpu().item()
+            
+            loss = loss_func(pred_y, batch_y)
+            loss.backward()
+
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=20)
+            total_norm = 0
+            parameters = [p for p in model.parameters() if p.grad is not None and p.requires_grad]
+            for p in parameters:
+                param_norm = p.grad.detach().data.norm(2)
+                total_norm += param_norm.item() ** 2
+            total_norm = total_norm ** 0.5
+            writer.add_scalar('gradient_norm', total_norm, (i + epoch * num_batches))
+
+            optimizer.step()
+
+            print(f'batch {i}/{num_batches}, loss {loss.item()}, mse_log10q {mse_log10q.item()}, mse_log10s {mse_log10s.item()}')
+            writer.add_scalar('loss/batch_loss', loss.item(), (i + epoch * num_batches))
+            writer.add_scalar('mse_batch/batch_mse_t0', mse[0], (i + epoch * num_batches))
+            writer.add_scalar('mse_batch/batch_mse_tE', mse[1], (i + epoch * num_batches))
+            writer.add_scalar('mse_batch/batch_mse_log10fs', mse[2], (i + epoch * num_batches))
+
+            if (i + epoch * num_batches) % 20 == 0:
+                model.eval()
+                torch.save({
+                'args': args,
+                'state_dict': model.state_dict(),
+                }, ckpt_path)
+                print(f'Model saved to {ckpt_path}')
+
+                with torch.no_grad():
+                    pred_y = model(test_coeffs)
+                    loss = loss_func(pred_y, test_Y)
+
+                    mse = torch.mean((batch_y - pred_y)**2, dim=-1).detach().cpu().item()
+
+                    pred_y_rand = model(test_rand_coeffs)
+                    loss_rand = loss_func(pred_y_rand, test_Y)
+
+                    mse_rand = torch.mean((batch_y - pred_y)**2, dim=-1).detach().cpu().item()
+
+                    message = f'Epoch {(i + epoch * num_batches)/20}, Test Loss {loss.item()}, mse_t0, tE, log10fs {mse}, loss_rand {loss_rand.item()}, mse_t0, tE, log10fs_rand {mse_rand}'
+                    writer.add_scalar('loss/test_loss', loss.item(), (i + epoch * num_batches)/20)
+                    writer.add_scalar('loss/test_loss_rand', loss_rand.item(), (i + epoch * num_batches)/20)
+                    writer.add_scalar('mse/test_mse_t0', mse[0], (i + epoch * num_batches)/20)
+                    writer.add_scalar('mse/test_mse_tE', mse[1], (i + epoch * num_batches)/20)
+                    writer.add_scalar('mse/test_mse_log10fs', mse[2], (i + epoch * num_batches)/20)
+                    writer.add_scalar('mse_rand/test_mse_t0', mse_rand[0], (i + epoch * num_batches)/20)
+                    writer.add_scalar('mse_rand/test_mse_tE', mse_rand[1], (i + epoch * num_batches)/20)
+                    writer.add_scalar('mse_rand/test_mse_log10fs', mse_rand[2], (i + epoch * num_batches)/20)
+                    for name, param in model.named_parameters():
+                        writer.add_histogram(name, param.clone().cpu().data.numpy(), (i + epoch * num_batches)/20)
+                    # logger.info("Experiment " + str(experimentID))
+                    logger.info(message)
+
+                model.train()
+
+    torch.save({
+        'args': args,
+        'state_dict': model.state_dict(),
+    }, ckpt_path)
+    print(f'Model saved to {ckpt_path}')
+    writer.close()
